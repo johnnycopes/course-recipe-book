26e74ec0d266bc38794b0aa3a47bd81c50716a9b
John Coppola <jcoppola@instien.com>
2019-01-08 14:45:53 -0500

    adds HTTPClientModule and sets up dummy Firebase to store/fetch recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+  }
+
+  onFetchData() {
+    this.recipeService.fetchRecipes();
+  }
+
+  onSaveData() {
+    this.recipeService.saveRecipes().subscribe(
+      (response) => console.log(response)
+    );
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
-import { Subject } from 'rxjs';
+import { Subject, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private readonly recipeUrl = 'https://max-recipe-book-104b7.firebaseio.com/recipes.json';
   private recipes: Recipe[] = [
     new Recipe(
       'Curry',
@@ -30,7 +33,7 @@ export class RecipeService {
     )
   ];
 
-  constructor() {}
+  constructor(private http: HttpClient) { }
 
   getRecipe(index: number) {
     return this.recipes[index];
@@ -53,5 +56,43 @@ export class RecipeService {
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice());
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>(this.recipeUrl)
+      .pipe(
+        catchError(this.handleError)
+      )
+      .subscribe(
+        (recipes) => {
+          this.recipes = recipes;
+          this.recipesChanged.next(this.recipes.slice());
+          console.log('fetch');
+        }
+      );
+  }
+
+  saveRecipes() {
+    return this.http.put<Recipe[]>(this.recipeUrl, this.recipes)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.'
+    );
   }
 }
